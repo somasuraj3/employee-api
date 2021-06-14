deb8167a78f51b02176ee86d750c99fef9fcf9e2
somasuraj <somasuraj@gmail.com>
2021-06-14 19:38:03 +0530

    Updated response message

--- src/main/java/com/cybage/myjavaapp/controller/DemoController.java
+++ src/main/java/com/cybage/myjavaapp/controller/DemoController.java
@@ -1,12 +1,15 @@
 package com.cybage.myjavaapp.controller;
 
+import com.cybage.myjavaapp.model.Message;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     @GetMapping("/")
-    public String GetDemoMessage() {
-        return "Hello, this is sample java microservice for Docker demo";
+    public ResponseEntity<Object> GetDemoMessage() {
+        return new ResponseEntity<Object>(new Message("This message is from Java microservice"), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/cybage/myjavaapp/model/Message.java
@@ -0,0 +1,24 @@
+package com.cybage.myjavaapp.model;
+
+public class Message {
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
